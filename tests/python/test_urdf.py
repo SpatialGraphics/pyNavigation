1957c7773c07e2afd4018ae609355e6b1e682648
yangfengzzz <yangfengzzz@hotmail.com>
2024-09-26 11:27:36 +0800

    save

--- tests/python/test_urdf.py
+++ tests/python/test_urdf.py
@@ -4,20 +4,10 @@
 #  personal capacity and am not conveying any rights to any intellectual
 #  property of any third parties.
 
-import pyPlan
-from pyPlan import urdf
+import pyNavigation
+import pytest
 import os
 
 
 def test_parser():
-    path = os.path.join(os.path.dirname(__file__),
-                        '../../../example-robot-data/robots/panda_description/urdf/panda.urdf')
-    model = urdf.parseURDFFile(path)
-    for name, joint in model.joints.items():
-        print(f"joint name: {name}")
-    for name, link in model.links.items():
-        print(f"link name: {name}")
-        if link.visual:
-            geometry = link.visual.geometry
-            if isinstance(geometry, urdf.Mesh):
-                print(f"mesh file: {geometry.filename}")
+    ...
